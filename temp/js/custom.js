468303b04e25ea28c43b296f954b2789dbbc6f0b
Erik Otárola-Castillo <eoc@purdue.edu>
2026-04-01 08:09:11 -0400

    cleaned version

--- temp/js/custom.js
+++ temp/js/custom.js
@@ -152,30 +152,32 @@ $(window).on("load",function (){
     $(".loading").fadeOut(500);
 
      // contact form
-    $('#contact-form').validator();
+    if ($('#contact-form').length && !$('#contact-form').attr('onsubmit')) {
+        $('#contact-form').validator();
 
-    $('#contact-form').on('submit', function (e) {
-        if (!e.isDefaultPrevented()) {
-            var url = "contact.php";
+        $('#contact-form').on('submit', function (e) {
+            if (!e.isDefaultPrevented()) {
+                var url = "pages/contact.php";
 
-            $.ajax({
-                type: "POST",
-                url: url,
-                data: $(this).serialize(),
-                success: function (data)
-                {
-                    var messageAlert = 'alert-' + data.type;
-                    var messageText = data.message;
+                $.ajax({
+                    type: "POST",
+                    url: url,
+                    data: $(this).serialize(),
+                    success: function (data)
+                    {
+                        var messageAlert = 'alert-' + data.type;
+                        var messageText = data.message;
 
-                    var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
-                    if (messageAlert && messageText) {
-                        $('#contact-form').find('.messages').html(alertBox);
-                        $('#contact-form')[0].reset();
+                        var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
+                        if (messageAlert && messageText) {
+                            $('#contact-form').find('.messages').html(alertBox);
+                            $('#contact-form')[0].reset();
+                        }
                     }
-                }
-            });
-            return false;
-        }
-    });
+                });
+                return false;
+            }
+        });
+    }
 
 });
