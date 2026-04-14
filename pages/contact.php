b509e8471b0271bae5639f71ba23ab37202e972b
dreoc <eoc@purdue.edu>
2026-04-14 14:47:32 -0400

    launch_update

--- pages/contact.php
+++ pages/contact.php
@@ -1,8 +1,8 @@
 <?php
 
 // configure
-$from = 'Demo contact form <demo@domain.com>';
-$sendTo = 'Demo contact form <demo@domain.com>'; // Add Your Email
+$from = 'The QuantLab <eoc@purdue.edu>';
+$sendTo = 'eoc@purdue.edu';
 $subject = 'New message from contact form';
 $fields = array('name' => 'Name', 'subject' => 'Subject', 'email' => 'Email', 'message' => 'Message'); // array variable name => Text to appear in the email
 $okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!';
