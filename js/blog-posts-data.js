e214563541a30bda7a3df3faaf44b2b8d1bdbb65
dreoc <eoc@purdue.edu>
2026-04-14 17:38:41 -0400

    updates

--- js/blog-posts-data.js
+++ js/blog-posts-data.js
@@ -40,8 +40,8 @@ window.quantLabBlogPosts = [
     "image": "../img/americas/americas_topomap.jpg",
     "cardImage": "../img/americas/americas_topomap.jpg",
     "excerpt": "Bayesian methods in archaeology are useful for much more than chronology. They let us formalize competing hypotheses, compare evidence, and quantify uncertainty directly.",
-    "url": "https://www.thequantlab.org/temp/pages/blog/erik-bayesian-archaeology.html",
-    "canonicalUrl": "https://www.thequantlab.org/temp/pages/blog/erik-bayesian-archaeology.html"
+    "url": "https://www.thequantlab.org/pages/blog/erik-bayesian-archaeology.html",
+    "canonicalUrl": "https://www.thequantlab.org/pages/blog/erik-bayesian-archaeology.html"
   },
   {
     "id": "volunteer-profile",
